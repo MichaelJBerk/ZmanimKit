0b8b7650f691ae18cc128185db7ac3ef4b1b5384
Dani Smith <ds1239@hotmail.com>
2016-09-02 12:07:42 -0400

    began work on documentation.

--- KosherSwift/Core/Calendar/Chagim/KSJewishCalendar.swift
+++ KosherSwift/Core/Calendar/Chagim/KSJewishCalendar.swift
@@ -7,14 +7,27 @@ import Foundation
 
 public class KSJewishCalendar: KSComplexZmanimCalendar
 {
-	var returnsModernHolidays : Bool = false
-	var inIsrael : Bool = false
+    /**
+     *  Determines if we consider "modern" holidays,
+     *  such as Yom Ha'atzmaut when returning values.
+     */
+	var returnsModernHolidays = false
+    
+    /**
+     *  Determines if we account for diaspora in our calculations.
+     */
+	var inIsrael = false
 	
 	override init (location aGeoLocation:KSGeoLocation)
 	{
 		super.init(location: aGeoLocation)
 	}
 	
+    /** 
+     * Returns the index of any holidays that fall on the date represented by `NSDate()`.
+     * 
+     * - returns: A yom tov if a yom tov falls on `NSDate()`, nil if not.
+     */
 	public func yomTovIndex() -> kYomimTovim?
 	{
 	    let useModernHolidays: Bool = returnsModernHolidays
@@ -201,6 +214,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return nil
 	}
 	
+    /** 
+     * This method determines if the given date is a Jewish Holiday.
+     *
+     * - returns: true if the given date is a yom tov, returns false if given date isn't a holiday, or is a fast day or chanukah.
+     */
 	public func isYomTov() -> Bool
 	{
         if let holidayIndex = yomTovIndex()
@@ -214,22 +232,42 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return yomTovIndex() != nil
 	}
 	
+    /**
+     * This method determines if the given date is Chol Hamoed of Pesach or Succos.
+     *
+     * - returns: true if given date is Chol Hamoed of Pesach or Succos.
+     */
 	public func isCholHamoed() -> Bool
 	{
 	    let holidayIndex = yomTovIndex()
 	    return (holidayIndex == .KSholHamoedPesach || holidayIndex == .KSholHamoedSuccos)
 	}
 	
+    /**
+     * This method determines if the given date is Chol Hamoed Succos.
+     *
+     * - returns: true if given date is Chol Hamoed Succos
+     */
 	public func isCholHamoedSuccos() -> Bool
 	{
 	    return (currentHebrewMonth() == .kTishrei) && ((currentHebrewDayOfMonth() >= 17 && currentHebrewDayOfMonth() <= 20) || (currentHebrewDayOfMonth() == 16 && inIsrael))
 	}
 	
+    /**
+     * This method determines if the given date is Chol Hamoed of Pesach.
+     *
+     *  - returns: true if the current day is Chol Hamoed of Pesach.
+     */
 	public func isCholHamoedPesach() -> Bool
 	{
 	    return currentHebrewMonth() == .kNissan && isCholHamoed()
 	}
 	
+    /**
+     *  This method determines if the current day is erev Yom Tov.
+     *
+     *  - returns: true if the current day is Erev - Pesach, Shavuos, Rosh Hashana, Yom Kippur or Succos.
+     */
 	public func isErevYomTov() -> Bool
 	{
 	    let holidayIndex = yomTovIndex()
@@ -237,11 +275,23 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return holidayIndex == .kErevPesach || holidayIndex == .kErevShavuos || holidayIndex == .kErevRoshHashana || holidayIndex == .kErevYomKippur || holidayIndex == .kErevSuccos
 	}
 	
+    /**
+     *  This method determines if the current day is Erev Rosh
+     *  Chodesh.
+     *
+     *  - returns: true if the current day is Erev Rosh Chodesh. Returns NO for Erev Rosh Hashana
+     */
 	public func isErevRoshChodesh() -> Bool
 	{
 	    return (currentHebrewDayOfMonth() == 29 && currentHebrewMonth() != .kElul)
 	}
 	
+    /**
+     *  This method determines if the day is a Taanis (fast day).
+     *
+     *  - returns: true if the current day is 17 of Tammuz, Tisha B'Av, Yom Kippur,
+     *  Fast of Gedalyah, 10 of Teves or the Fast of Esther.
+     */
 	public func isTaanis() -> Bool
 	{
 	    let holidayIndex = yomTovIndex()
@@ -249,6 +299,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return holidayIndex == .kSeventeenthOfTammuz || holidayIndex == .kTishaBeav || holidayIndex == .kYomKippur || holidayIndex == .kFastOfGedalya || holidayIndex == .kTenthOfTeves || holidayIndex == .kFastOfEsther
 	}
 	
+    /**
+     *  This method determines the day of Chanukah.
+     *
+     *  - returns: NSInteger equal to the number of the day of Chanukah, or -1 if it is not Chanukah
+     */
 	public func dayOfChanukah() -> Int
 	{
 	    if isChanukah()
@@ -268,36 +323,72 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    }
 	}
 	
+    /**
+     *  This method determines if the date is Chanukkah
+     *
+     *  - returns: true if it is Chanukah
+     */
 	public func isChanukah() -> Bool
 	{
 	    return yomTovIndex() == .KShanukah
 	}
 	
+    /**
+     *  This method determines if Purim
+     *
+     *  - returns: true if the day is Purim
+     */
 	public func isPurim() -> Bool
 	{
 	    return yomTovIndex() == .kPurim
 	}
 	
+    /**
+     *  This method determines if the current day is Rosh
+     *  Chodesh.
+     *
+     *  - returns: true if the current day is Rosh Chodesh. Returns false for Rosh Hashana
+     */
 	public func isRoshChodesh() -> Bool
 	{
 	    return (currentHebrewDayOfMonth() == 1 && currentHebrewMonth() != .kTishrei) || currentHebrewDayOfMonth() == 30
 	}
 	
+    /**
+     *  This method determines if the date is Pesach
+     *
+     *  - returns: true if it is Pesach
+     */
 	public func isPesach() -> Bool
 	{
 	    return yomTovIndex() == .kPesach
 	}
 	
+    /**
+     *  This method determines if the date is Shavuos
+     *
+     *  - returns: true if it is Shavuos
+     */
 	public func isShavuos() -> Bool
 	{
 	    return yomTovIndex() == .kShavuos
 	}
 	
+    /**
+     *  This method determines if the date is Succos
+     *
+     *  - returns: true if it is Succos
+     */
 	public func isSuccos() -> Bool
 	{
 	    return yomTovIndex() == .kSuccos
 	}
 	
+    /**
+     *  This method determines if the date is Simchat Torah
+     *
+     *  - returns: true if it is Simchat Torah
+     */
 	public func isSimchasTorah() -> Bool
 	{
 	    if currentHebrewMonth() == .kTishrei &&
@@ -309,6 +400,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return false
 	}
 	
+    /**
+     *  This method determines if the date is Shmini Atzeres
+     *
+     *  - returns: true if it is Shmini Atzeres
+     */
 	public func isShminiAtzeres() -> Bool
 	{
 	    return (currentHebrewMonth() == .kTishrei && currentHebrewDayOfMonth() == 22)
@@ -367,12 +463,33 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return moladByAddingMonthsToTohu(Int(monthDifference))
 	}
 	
+    /**
+     *  The traditional calculation uses local time. This
+     *  method subtracts 20.94 minutes (20 minutes and 56.496 seconds)
+     *  from the local time (Har Habayis with a longitude
+     *  of 35.2354&deg; is 5.2354&deg; away from the %15 timezone longitude)to get to standard time.
+     *  This method intentionally uses standard time and not daylight savings time.
+     *  (Java will implicitly format the time to the default (or set)Timezone) -> is this relevant to Objective C?
+     *
+     *  - parameter month: An Int representing the month. This method expects 1 for Nissan and 13 for Adar II. Use the constants to avoid any confusion.
+     *  - parameter year: The Hebrew year to use in calculating.
+     *
+     *  - returns: The molad in Standard Time in Yerushalayim as a NSDate.
+     */
 	public func moladAsDateForMonth(month: Int, ofYear year: Int) -> NSDate
 	{
 	    let dateFromMonthAndYear: NSDate = NSDate.dateWithHebrewMonth(month, andDay: 1, andYear: year)
 	    return moladForDate(dateFromMonthAndYear)
 	}
 	
+    /**
+     *  Currently returns the time even if it is during the day. It should return
+     *  the 72 Minute Tzais after to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter date: The date to calculate for.
+     *
+     *  - returns: Earliest time of Kiddush Levana calculated as 3 days after the molad.
+     */
 	public func tchilasZmanKidushLevana3DaysForDate(date: NSDate) -> NSDate
 	{
 	    let molad: NSDate = moladForDate(date)
@@ -383,12 +500,29 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return moladForDate(molad)
 	}
 	
+    /**
+     *  Currently returns the time even if it is during the day. It should return
+     *  the 72 Minute Tzais after to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter month: The Hebrew month to use in calculating.
+     *  - parameter year: The Hebrew year to use in calculating.
+     *
+     *  - returns: Earliest time of Kiddush Levana calculated as 3 days after the molad.
+     */
 	public func tchilasZmanKidushLevana3DaysForMonth(month: Int, ofYear year: Int) -> NSDate
 	{
 	    let dateFromMonthAndYear: NSDate = NSDate.dateWithHebrewMonth(month, andDay: 1, andYear: year)
 	    return tchilasZmanKidushLevana3DaysForDate(dateFromMonthAndYear)
 	}
 	
+    /**
+     *  Currently returns the time even if it is during the day. It should return
+     *  the 72 Minute Tzais after to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter date: The date to calculate for.
+     *
+     *  - returns: Earliest time of Kiddush Levana calculated as 7 days after the molad.
+     */
 	public func tchilasZmanKidushLevana7DaysForDate(date: NSDate) -> NSDate
 	{
 	    let molad: NSDate = moladForDate(date)
@@ -399,12 +533,33 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return moladForDate(molad)
 	}
 	
+    /**
+     *  Currently returns the time even if it is during the day. It should return
+     *  the 72 Minute Tzais after to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter month: The Hebrew month to use in calculating.
+     *  - parameter year: The Hebrew year to use in calculating.
+     *
+     *  - returns: Earliest time of Kiddush Levana calculated as 7 days after the molad.
+     */
 	public func tchilasZmanKidushLevana7DaysForMonth(month: Int, ofYear year: Int) -> NSDate
 	{
 	    let dateFromMonthAndYear: NSDate = NSDate.dateWithHebrewMonth(month, andDay: 1, andYear: year)
 	    return tchilasZmanKidushLevana7DaysForDate(dateFromMonthAndYear)
 	}
 	
+    /**
+     *  Calculates the latest time of Kiddush Levana according to the
+     *  Maharil's opinion that it is calculated as halfway between molad and molad.
+     *  This adds half the 29 days, 12 hours and 793 chalakim time between molad and
+     *  molad (14 days, 18 hours, 22 minutes and 666 milliseconds)to the month's molad.
+     *
+     *  - discussion: Currently returns the time even if it is during the day. It should return the 72 Minute Alos prior to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter date: The date to calculate for.
+     *
+     *  - returns: Latest time of Kiddush Levana between Moldos
+     */
 	public func sofZmanKidushLevanaBetweenMoldosForDate(date: NSDate) -> NSDate
 	{
 	    var molad: NSDate = moladForDate(date)
@@ -418,35 +573,84 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return molad
 	}
 	
+    /**
+     *  Calculates the latest time of Kiddush Levana according to the
+     *  Maharil's opinion that it is calculated as halfway between molad and molad.
+     *  This adds half the 29 days, 12 hours and 793 chalakim time between molad and
+     *  molad (14 days, 18 hours, 22 minutes and 666 milliseconds)to the month's molad.
+     *
+     *  - discussion: Currently returns the time even if it is during the day. It should return the 72 Minute Alos prior to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter month: The Hebrew month to use in calculating.
+     *  - parameter year: The Hebrew year to use in calculating.
+     *
+     *  - returns: Latest time of Kiddush Levana between Moldos
+     */
 	public func sofZmanKidushLevanaBetweenMoldosForMonth(month: Int, ofYear year: Int) -> NSDate
 	{
 	    let dateFromMonthAndYear: NSDate = NSDate.dateWithHebrewMonth(month, andDay: 1, andYear: year)
 	    return sofZmanKidushLevanaBetweenMoldosForDate(dateFromMonthAndYear)
 	}
 	
+    /**
+     *  This is the opinion brought down in the Shulchan Aruch (Orach Chaim 426).
+     *  It should be noted that some opinions hold that the Rema who brings down the opinion
+     *  of the Maharil's of calculating half way between molad and molad is of the opinion
+     *  that the Mechaber agrees to his opinion. Also see the Aruch Hashulchan.
+     *  For additional details on the subject, See Rabbi Dovid Heber's very detailed writeup
+     *  in Siman Daled (chapter 4)of Shaarei Zmanim http://www.worldcat.org/oclc/461326125.
+     *
+     *  - discussion Currently returns the time even if it is during the day. It should return the  Alos prior to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter date: The date to calculate for.
+     *
+     *  - returns: Latest time of Kiddush Levana calculated as 15 days after the molad.
+     */
 	public func sofZmanKidushLevana15DaysForDate(date: NSDate) -> NSDate
 	{
-	    //  Get the molad
-	    
 	    let molad: NSDate = moladForDate(date)
-	    
 	    let hebrewCalendar: NSCalendar = NSCalendar(calendarIdentifier:NSCalendarIdentifierHebrew)!
 	    return hebrewCalendar.dateByAddingDays(15, toDate: molad)
 	}
 	
+    /**
+     *  This is the opinion brought down in the Shulchan Aruch (Orach Chaim 426).
+     *  It should be noted that some opinions hold that the Rema who brings down the opinion
+     *  of the Maharil's of calculating half way between molad and molad is of the opinion
+     *  that the Mechaber agrees to his opinion. Also see the Aruch Hashulchan.
+     *  For additional details on the subject, See Rabbi Dovid Heber's very detailed writeup
+     *  in Siman Daled (chapter 4)of Shaarei Zmanim http://www.worldcat.org/oclc/461326125.
+     *
+     *  Note: Currently returns the time even if it is during the day.
+     *  It should return the  Alos prior to the time if the zman is between Alos and Tzais.
+     *
+     *  - parameter month: The Hebrew month to use in calculating.
+     *  - parameter year: The Hebrew year to use in calculating.
+     *
+     *  - returns: Latest time of Kiddush Levana calculated as 15 days after the molad.
+     */
 	public func sofZmanKidushLevana15DaysForMonth(month: Int, ofYear year: Int) -> NSDate
 	{
 	    let dateFromMonthAndYear: NSDate = NSDate.dateWithHebrewMonth(month, andDay: 1, andYear: year)
 	    return sofZmanKidushLevana15DaysForDate(dateFromMonthAndYear)
     }
 	
+    /**
+     *  Returns the Daf Yomi (Bavli) for the date the calendar is set to
+     *
+     *  - returns: KCDaf object corresponding to the date
+     */
 	public func dafYomiBavli() -> KSDaf
 	{
 	    let calculator: KSDafYomiCalculator = KSDafYomiCalculator(date: workingDate!)
-	
 	    return calculator.dafYomiBavliForDate(workingDate!)!
 	}
 	
+    /**
+     *  Returns the current hebrew month
+     *
+     *  - returns: Int corresponding to the current Hebrew month
+     */
 	public func currentHebrewMonth() -> kHebrewMonth
 	{
 	    let hebrewCalendar: NSCalendar = NSCalendar(calendarIdentifier:NSCalendarIdentifierHebrew)!
@@ -454,6 +658,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
         return kHebrewMonth(rawValue: hebrewCalendar.components(NSCalendarUnit.Month, fromDate:workingDateAdjustedForSunset()).month)!
 	}
 	
+    /**
+     *  Returns the day of the current hebrew month
+     *
+     *  - returns: Int corresponding to the day of the current Hebrew month
+     */
 	public func currentHebrewDayOfMonth() -> Int
 	{
 	    let now: NSDate = workingDateAdjustedForSunset()
@@ -462,6 +671,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return hebrewCalendar.components(NSCalendarUnit.Day, fromDate:now).day
 	}
 	
+    /**
+     *  Returns the current day of the civil week (i.e. days starting @ midnight)
+     *
+     *  - returns: Int corresponding to the current day of the civil week
+     */
 	public func currentDayOfTheWeek() -> Int
 	{
 	    let now: NSDate = workingDate!
@@ -470,6 +684,11 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return gregorianCalendar.components(NSCalendarUnit.Weekday, fromDate:now).weekday
 	}
 	
+    /**
+     *  Determine if the current Hebrew year is a leap year
+     *
+     *  - returns: true if it is a leap year
+     **/
 	public func isCurrentlyHebrewLeapYear() -> Bool
 	{
 	    let now: NSDate = workingDateAdjustedForSunset()
@@ -479,16 +698,33 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return isHebrewLeapYear(year)
 	}
 	
+    /**
+     *  Determine if the parameter Hebrew year is a leap year
+     *
+     *  - parameter year: A hebrew year.
+     *
+     *  - returns: true if it is a leap year
+     **/
 	public func isHebrewLeapYear(year: Int) -> Bool
 	{
 		return ((7 * year + 1) % 19) < 7
 	}
 	
+    /**
+     *  Determine if the month of Kislev is short this year.
+     *
+     *  - returns: true if Kislef is short
+     **/
 	public func isKislevShort() -> Bool
 	{
 	    return lengthOfHebrewYear(currentHebrewYear()) % 10 == 3
 	}
 	
+    /**
+     *  Returns the current Hebrew Year
+     *
+     *  - returns: Int representing the current Hebrew year.
+     **/
 	public func currentHebrewYear() -> Int
 	{
 	    let now: NSDate = workingDateAdjustedForSunset()
@@ -497,6 +733,13 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return hebrewCalendar.components(NSCalendarUnit.Year, fromDate:now).year
 	}
 	
+    /**
+     *  Returns the length of the parameter Hebrew year
+     *
+     *  - parameter year: A hebrew year.
+     *
+     *  - returns: Int corresponding to the number of days of the year
+     **/
 	public func lengthOfHebrewYear(year: Int) -> Int
 	{
 		let hebrewCalendar = NSCalendar(calendarIdentifier:NSCalendarIdentifierHebrew)!
@@ -533,6 +776,13 @@ public class KSJewishCalendar: KSComplexZmanimCalendar
 	    return KShalakimMoladTohu + (KShalakimPerMonth * monthsElapsed)
 	}
 	
+    /**
+     *  Returns the friday following a given date
+     *
+     *  - parameter workingDate The date to calculate for.
+     *
+     *  - returns: The friday folling the provided date.
+     **/
 	public func fridayFollowingDate(workingDate: NSDate) -> NSDate
 	{
 	    let gregorianCalendar: NSCalendar = NSCalendar(calendarIdentifier:NSCalendarIdentifierGregorian)!
